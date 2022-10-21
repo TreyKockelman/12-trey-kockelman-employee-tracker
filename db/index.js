07762449bda8cd7090e816dddb95081bf5bedc81
TreyKockelman <kockelmant@gmail.com>
2022-10-21 18:56:28 -0500

    added employee, role, and department add functions

--- db/index.js
+++ db/index.js
@@ -10,12 +10,39 @@ class DB {
   // THIS IS NOT THE FINAL QUERY. YOU WILL NEED TO MODIFY THIS QUERY SO THAT YOU JOIN 
   // THE EMPLOYEES WITH THEIR ROLES, SALARIES, DEPARTMENTS, AND MANAGERS
   // HINT: A TABLE CAN BE JOINED ON ITSELF WITH PROPER TABLE ALIASING
+
+  findAllDepartments() {
+    return this.connection.promise().query(
+      "SELECT * FROM department;"
+    );
+  }
+
+  findAllRoles() {
+    return this.connection.promise().query(
+      `SELECT role.id, role.title, role.salary, department.name AS department_name
+      FROM department
+      INNER JOIN role on role.department_id=department.id;`
+    );
+  }
   
   findAllEmployees() {
     return this.connection.promise().query(
-      "SELECT * FROM employee;"
+      `SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department, CONCAT(manager.first_name, " ", manager.last_name) AS manager
+      FROM employee
+      LEFT JOIN role ON role.id=employee.role_id
+      LEFT JOIN department ON department.id=role.department_id
+      LEFT JOIN employee manager ON manager.id=employee.manager_id`
     );
   }
+
+  addDepartment(deptName) {
+    return this.connection.promise().query("INSERT INTO department(name) VALUES(?)", deptName)};
+
+  addRole(title, salary, depID) {
+    return this.connection.promise().query("INSERT INTO role(title, salary, department_id) VALUES(?, ?, ?)", [title, salary, depID])};
+
+  addEmployee(firstname, lastname, role, manager) {
+    return this.connection.promise().query("INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES(?, ?, ?, ?)", [firstname, lastname, role, manager])};
 
   // Add more class methods below for all the database operations needed.
   // Sometimes you may need to pass an id value into a method so it knows 
@@ -33,8 +60,5 @@ class DB {
 
   
 }
-
-
-
 
 module.exports = new DB(connection);
--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const db = require("./db");
 // Use this function to display the ascii art logo and to begin the main prompts
 function init() {
 
-
   loadMainPrompts()
 }
 
@@ -24,11 +23,33 @@ function loadMainPrompts() {
       message: "What would you like to do?",
       choices: [
         {
+          name: "View All Departments",
+          value: "VIEW_DEPARTMENTS"
+        },
+        {
+          name: "View All Roles",
+          value: "VIEW_ROLES"
+        },
+        {
           name: "View All Employees",
           value: "VIEW_EMPLOYEES"
         },
-
-        // add more options here
+        {
+          name: "Add a Department",
+          value: "ADD_DEPARTMENT"
+        },
+        {
+          name: "Add a Role",
+          value: "ADD_ROLE"
+        },
+        {
+          name: "Add an Employee",
+          value: "ADD_EMPLOYEE"
+        },
+        {
+          name: "Update Employee Role",
+          value: "UPDATE_EMPLOYEE_ROLE"
+        }
       ]
     }
   ]).then(res => {
@@ -36,11 +57,24 @@ function loadMainPrompts() {
     // Call the appropriate function depending on what the user chose
     
     switch (choice) {
+      case "VIEW_DEPARTMENTS":
+        viewDepartments();
+        break;
+      case "VIEW_ROLES":
+        viewRoles();
+        break;
       case "VIEW_EMPLOYEES":
         viewEmployees();
         break;
-      
-        // add the other case statements here
+      case "ADD_DEPARTMENT":
+        addDepartment();
+        break;
+      case "ADD_ROLE":
+        addRole();
+        break;
+      case "ADD_EMPLOYEE":
+        addEmployee();
+        break;
     }
   }
 )}
@@ -48,13 +82,29 @@ function loadMainPrompts() {
 
 
 
-/* ======= Controllers ============================================================ */
+/* ======= Controllers ========= */
 
-// Here is a function which handles the first prompt option:  View all employees
+function viewDepartments() {
+  db.findAllDepartments()
+    .then(([rows]) => {
+      let departments = rows;
+      console.log("\n");
+      console.table(departments);
+  })
+  .then(() => loadMainPrompts());
+};
+
+function viewRoles() {
+  db.findAllRoles() 
+    .then(([rows]) => {
+      let role = rows;
+      console.log("\n");
+      console.table(role);
+    })
+  .then(() => loadMainPrompts());
+}
+
 function viewEmployees() {
-
-  // Here we call the method in the db file for finding all employees.
-  // we get the result back, and then display the result 
   db.findAllEmployees()
     .then(([rows]) => {
       let employees = rows;
@@ -64,6 +114,69 @@ function viewEmployees() {
     .then(() => loadMainPrompts());
 }
 
+function addDepartment() {
+  prompt(
+    {
+      type: 'input',
+      message: "Enter Department Name",
+      name: 'dep_name',
+    },
+  ).then(res => {
+    db.addDepartment(res.dep_name).then(() => loadMainPrompts());
+  })
+};
+
+function addRole() {
+  prompt([
+    {
+      type: 'input',
+      message: "Enter Role Name",
+      name: 'role_name',
+    },
+    {
+      type: 'input',
+      message: "Enter Role Salary",
+      name: 'role_salary',
+    },
+    {
+      type: 'list',
+      message: "Enter Role Department ID",
+      choices: [{name: "Sales", value: 1}, {name: "Engineering", value: 2}, {name: "Finance", value: 3}, {name: "Legal", value: 4}],
+      name: 'role_depID',
+    },
+  ]).then(res => {
+    db.addRole(res.role_name, res.role_salary, res.role_depID).then(() => loadMainPrompts());
+  })
+};
+
+function addEmployee() {
+  prompt([
+    {
+      type: 'input',
+      message: "Enter Employee First Name",
+      name: 'first_name',
+    },
+    {
+      type: 'input',
+      message: "Enter Employee Last Name",
+      name: 'last_name',
+    },
+    {
+      type: 'list',
+      message: "Enter Employee Role",
+      choices: [{name: "Sales Lead", value: 1}, {name: "Salesperson", value: 2}, {name: "Lead Engineer", value: 3}, {name: "Software Engineer", value: 4}, {name: "Account Manager", value: 5}, {name: "Accountant", value: 6}, {name: "Legal Team Lead", value: 7}, {name: "Lawyer", value: 8}],
+      name: 'role',
+    },
+    {
+      type: 'list',
+      message: "Enter Employee Manager",
+      choices: [{name: "John Doe", value: 1}, {name: "Ashley Rodriguez", value: 3}, {name: "Malia Brown", value: 5}, {name: "Sarah Lourd", value: 7}],
+      name: 'manager',
+    },
+  ]).then(res => {
+    db.addEmployee(res.first_name, res.last_name, res.role, res.manager).then(() => loadMainPrompts());
+  })
+}
 
 
 /* ======= END Controllers ============================================================ */
